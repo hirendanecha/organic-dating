24cd21cb081a561dec278e1d910400388035a0b9
prashant <ppo.opash@gmail.com>
2023-12-28 17:08:04 +0530

    Change Page Modal

--- src/app/layouts/main-layout/pages/home/home.component.ts
+++ src/app/layouts/main-layout/pages/home/home.component.ts
@@ -93,8 +93,8 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
         if (name) {
           this.communitySlug = name;
           this.getCommunityDetailsBySlug();
-        // } else {
-        //   this.sharedService.advertizementLink = [];
+        } else {
+          this.sharedService.advertizementLink = [];
         }
 
         this.isNavigationEnd = true;
@@ -213,7 +213,11 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
           this.spinner.hide();
           if (res?.Id) {
             const details = res;
-        
+            if (res.pageType === 'page') {
+              this.sharedService.getAdvertizeMentLink(res?.Id);
+            } else {
+              this.sharedService.advertizementLink = null;
+            }
             const data = {
               title: details?.CommunityName,
               url: `${environment.webUrl}${details?.pageType}/${details?.slug}`,
@@ -418,6 +422,8 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
         keyboard: false,
         size: 'lg',
       });
+      data.link1 = this.sharedService?.advertizementLink[0]?.url;
+      data.link2 = this.sharedService?.advertizementLink[1]?.url;
     
     }
     modalRef.componentInstance.title = `Edit ${data.pageType} Details`;
@@ -603,6 +609,9 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
         const bytes = copyImage.length;
         const megabytes = bytes / (1024 * 1024);
         if (megabytes > 1) {
+          let copyImageTag = '<img\\s*src\\s*=\\s*""\\s*alt\\s*="">'
+          this.postData['postdescription'] = `<div>${content.replace(copyImage, '').replace(/\<br\>/ig, '').replace(new RegExp(copyImageTag, 'g'), '')}</div>`;
+          // this.postData['postdescription'] = content.replace(copyImage, '');
           this.postData['postdescription'] = content.replace(copyImage, '');
           const base64Image = copyImage
             .trim()
