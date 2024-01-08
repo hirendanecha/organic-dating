cd7af0e2bee8df42e5b65520514c320964e59ea3
prashant <ppo.opash@gmail.com>
2024-01-08 18:49:49 +0530

    Add pop up on edit post

--- src/app/@shared/components/post-card/post-card.component.ts
+++ src/app/@shared/components/post-card/post-card.component.ts
@@ -80,6 +80,8 @@ export class PostCardComponent implements OnInit {
   commentDescriptionimageUrl: string;
   replayCommentDescriptionimageUrl: string;
   shareButton = false;
+  isViewProfile = false;
+
 
   constructor(
     private seeFirstUserService: SeeFirstUserService,
@@ -98,6 +100,7 @@ export class PostCardComponent implements OnInit {
     public breakpointService: BreakpointService,
     public activeModal: NgbActiveModal
   ) {
+    this.router
     this.profileId = localStorage.getItem('profileId');
     afterNextRender(() => {
 
@@ -134,6 +137,8 @@ export class PostCardComponent implements OnInit {
     if (path === 'view-profile/:id' || path === 'post/:id') {
       this.shareButton = true;
     }
+    this.isViewProfile = path.includes('view-profile') || false;
+
   }
   getPostUrl(post: any) {
     // if (post.streamname) {
@@ -242,7 +247,7 @@ export class PostCardComponent implements OnInit {
   }
 
   editComment(comment): void {
-    if (comment.parentCommentId) {
+    if (comment){
       const modalRef = this.modalService.open(ReplyCommentModalComponent, {
         centered: true,
       });
@@ -257,7 +262,9 @@ export class PostCardComponent implements OnInit {
           this.commentData.postId = res?.postId;
           this.commentData.profileId = res?.profileId;
           this.commentData['id'] = res?.id;
-          this.commentData.parentCommentId = res?.parentCommentId;
+          if (res?.parentCommentId) {
+            this.commentData.parentCommentId = res?.parentCommentId;
+          }
           this.commentData['file'] = res?.file;
           this.commentData['imageUrl'] = res?.imageUrl
           
@@ -284,6 +291,8 @@ export class PostCardComponent implements OnInit {
   deletePost(post): void {
     const modalRef = this.modalService.open(ConfirmationModalComponent, {
       centered: true,
+      backdrop: 'static',
+
     });
     modalRef.componentInstance.title = 'Delete Post';
     modalRef.componentInstance.confirmButtonLabel = 'Delete';
