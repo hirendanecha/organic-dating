2a765b4be9ccd188cf8ca0c82ffae0b35d6978a9
prashant <ppo.opash@gmail.com>
2024-05-21 16:22:25 +0530

    Change wording

--- src/app/@shared/services/seo.service.ts
+++ src/app/@shared/services/seo.service.ts
@@ -69,7 +69,7 @@ export class SeoService {
     }
 
     if (!tagValue.image) {
-      tagValue.image = 'https://freedom.buzz/assets/images/banner/Healing-Tube-Logo.png';
+      tagValue.image = 'https://organic.dating/assets/images/banner/Healing-Tube-Logo.png';
     }
 
     this.meta.updateTag({
